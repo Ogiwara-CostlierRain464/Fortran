bdcace3cc569b25d2afd56f48562ef0f4f7abbed
ogiwara <yushiogiwara@gmail.com>
2019-09-28 20:49:32 +0900

    update to file I/O

--- SolarSystem.f90
+++ SolarSystem.f90
@@ -14,15 +14,22 @@ program SolarSystem
         character(256) :: name
     end type AstornomicalObject
 
-    type(AstornomicalObject) :: earth
-
-    earth%posi = Vector2d(1.496d11, 0d0)
-    earth%velo = Vector2d(0d0,29.78d3)
-    earth%accl = Vector2d(0d0,0d0)
-    earth%mass = 5.97d24
-    earth%name = "Earth"
     
-    print '(A,F20.1,A,F20.1,A)',"position of Earth=(", earth%posi%x, ",", earth%posi%y, ")"
-    read*
+    integer, parameter :: input_unit = 149              !入力に用いる装置番号
+    integer, parameter :: output_unit = 697             !出力に用いる装置番号
+    real(8), parameter :: AstornomicalUnit = 1.496d11 !天文単位
+
+    type(AstornomicalObject) :: earth
+    open(unit = input_unit, file="planet.dat",action="read")
+    read(input_unit,*) earth%name,earth%posi%x,earth%mass,earth%velo%y
+    earth%posi%y = 0d0
+    earth%velo%x = 0d0
+    earth%accl = Vector2d(0d0,0d0)
+    close(input_unit)
+
+    open(unit=output_unit,file=trim(earth%name)//'.txt',action="write")
+    write(output_unit, '(2F10.1)') earth%posi%x/AstornomicalUnit, earth%posi%y/AstornomicalUnit
+    close(output_unit)
+
 
 end program SolarSystem
